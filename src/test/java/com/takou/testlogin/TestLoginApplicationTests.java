0002fe3dd1f2c41c17a816eec3ae3e9e8857cb9b
TakouIbrahim <nonomoyenn@gmail.com>
2023-09-30 17:51:29 +0200

    first commit

--- src/test/java/com/takou/testlogin/TestLoginApplicationTests.java
+++ src/test/java/com/takou/testlogin/TestLoginApplicationTests.java
@@ -1,5 +1,7 @@
 package com.takou.testlogin;
 
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,9 @@ class TestLoginApplicationTests {
 
     @Test
     void contextLoads() {
+
+        Assertions.assertEquals(New );
+
     }
 
 }
